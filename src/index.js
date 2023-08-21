174ba80ccf45b9e4681806250dd740ef69cdf7c9
Anna Borovaya <any.georg@gmail.com>
2023-08-21 12:26:37 +0200

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let num = String(n);
+  let i = num.length;
+  let result = '';
+
+  while (i > 0) {
+    result = result + num[i - 1];
+    i = i - 1;
+  }
+  return parseInt(result);
 }
